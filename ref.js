63e0915a171b1b99737a49fa3143567d4d6ca738
Kayonga Chiteta <kayongachiteta01@gmail.com>
2022-04-30 15:48:04 +0200

    Fix linters.

--- ref.js
+++ ref.js
@@ -2,7 +2,6 @@ class BookLists {
   constructor() {
     this.bkList = JSON.parse(localStorage.getItem('library')) || [];
   }
-
   displayBooks() {
     const bookTable = document.getElementById('tb-body');
     let table = '';
@@ -18,38 +17,31 @@ class BookLists {
     });
     bookTable.innerHTML = table;
   }
-
   addBook(title, author) {
     const book = {
       id: this.bkList.length + 1,
       title,
       author,
     };
-
     this.bkList = [...this.bkList, book];
     localStorage.setItem('library', JSON.stringify(this.bkList));
   }
-
   removeBook(index) {
     this.bkList = this.bkList.filter((b) => b.id !== index);
     localStorage.setItem('library', JSON.stringify(this.bkList));
     this.displayBooks();
   }
 }
-
 const bkList = new BookLists();
 const titleInput = document.querySelector('#title');
 const authorInput = document.querySelector('#author');
 const addInput = document.querySelector('#button');
-
 bkList.displayBooks();
-
 // eslint-disable-next-line no-unused-vars
 function remove(index) {
     //eslint-disable-line
   bkList.removeBook(index);
 }
-
 addInput.addEventListener('click', (e) => {
   e.preventDefault();
   if (titleInput.value && authorInput.value) {
