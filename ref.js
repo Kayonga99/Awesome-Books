f96d8cf47321dbd9ab4a32b9e4a4af71601f3f6b
Kayonga Chiteta <kayongachiteta01@gmail.com>
2022-04-30 15:21:30 +0200

    A new js file.

--- ref.js
+++ ref.js
@@ -60,29 +60,4 @@ addInput.addEventListener('click', (e) => {
   }
 });
 
-// navigation
-const addButton = document.querySelector('.new-book');
-const see = document.querySelector('.list-');
-const contact = document.querySelector('.contact-us');
-const addNewBook = document.getElementById('form');
-const wiewList = document.querySelector('.tablesid');
-const viewContact = document.querySelector('.contact');
 
-addButton.addEventListener('click', () => {
-  addNewBook.style.display = 'block';
-  viewContact.style.display = 'none';
-  wiewList.style.display = 'none';
-});
-
-see.addEventListener('click', () => {
-  wiewList.classList.remove('tablesid');
-  addNewBook.style.display = 'none';
-  viewContact.style.display = 'none';
-  wiewList.style.display = 'table';
-});
-
-contact.addEventListener('click', () => {
-  viewContact.style.display = 'block';
-  addNewBook.style.display = 'none';
-  wiewList.style.display = 'none';
-});
