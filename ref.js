cd3ae93b0bc53126101973c690c9f6beddded11e
Kayonga Chiteta <kayongachiteta01@gmail.com>
2022-04-30 12:53:36 +0200

    Fix linter errors.

--- ref.js
+++ ref.js
@@ -62,22 +62,21 @@ addInput.addEventListener('click', (e) => {
 
 // website with navigation
 
-const addButton = document.querySelector(".new-book")
-const see = document.querySelector(".list-")
-const contact = document.querySelector(".contact-us")
-const addNewBook = document.getElementById('form')
-const wiewList = document.querySelector(".tablesid")
-const viewContact = document.querySelector(".contact")
-
+const addButton = document.querySelector('.new-book');
+const see = document.querySelector('.list-');
+const contact = document.querySelector('.contact-us');
+const addNewBook = document.getElementById('form');
+const wiewList = document.querySelector('.tablesid');
+const viewContact = document.querySelector('.contact');
 
 addButton.addEventListener('click', () => {
-  addNewBook.style.display = 'block'
-})
+  addNewBook.style.display = 'block';
+});
 
 see.addEventListener('click', () => {
-  wiewList.classList.remove('tablesid')
-})
+  wiewList.classList.remove('tablesid');
+});
 
 contact.addEventListener('click', () => {
-  viewContact.style.display = 'block'
-})
+  viewContact.style.display = 'block';
+});
