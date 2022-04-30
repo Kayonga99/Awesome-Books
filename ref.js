a7c754e866570eafaa90c72140267ea703de7a58
Kayonga Chiteta <kayongachiteta01@gmail.com>
2022-04-30 13:45:28 +0200

    Add function and fix eslint errors.

--- ref.js
+++ ref.js
@@ -71,12 +71,19 @@ const viewContact = document.querySelector('.contact');
 
 addButton.addEventListener('click', () => {
   addNewBook.style.display = 'block';
+  viewContact.style.display = 'none';
+  wiewList.style.display = 'none';
 });
 
 see.addEventListener('click', () => {
   wiewList.classList.remove('tablesid');
+  addNewBook.style.display = 'none';
+  viewContact.style.display = 'none';
+  wiewList.style.display = 'table';
 });
 
 contact.addEventListener('click', () => {
   viewContact.style.display = 'block';
+  addNewBook.style.display = 'none';
+  wiewList.style.display = 'none';
 });
